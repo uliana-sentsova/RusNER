50f7ac7bf08fbafd33955956cec6b2fd26803b30
uliana-sentsova <uliana.sentsova@gmail.com>
2017-02-25 22:21:41 +0100

    search from leaves implemented

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ script_path = "/Users/ulyanasidorova/Development/ru-syntax/ru-syntax.py"
 in_path = "/Users/ulyanasidorova/Development/RusNER/test.txt"
 out_path = "/Users/ulyanasidorova/Development/ru-syntax/out/result.txt"
 syntax_call_line = "python3"
-call(["python3", script_path, "-o", out_path, in_path])
+# call(["python3", script_path, "-o", out_path, in_path])
 
 sentences = []
 prev = 0
@@ -34,17 +34,32 @@ for sent in sentences:
         tree.add_elem(token.number, token)
     trees.append(tree)
 
-tree = trees[3]
-print([(token.surface, token.pos, token.parent) for token in tree.array])
+# 6,5,12 – проблемные
+tree = trees[6]
+print([(token.surface, token.syntax, token.parent, token.pos) for token in tree.array])
 
-
+# Поиск снизу:
 NPs = {}
-in_NP = []
-root = tree.get_root()
-print(root.surface)
-children = tree.get_children(root.number)
-print([ch.surface for ch in children])
-for child in children:
-    if child.pos == "S":
-        NP = tree.get_all_children(child.number)
-        print([child.surface] + [n.surface for n in NP])
+for token in tree.array:
+    if token.pos == "S":
+        children = tree.get_all_children(token.number)
+        if not children:
+            continue
+
+        phrase = [token] + children
+        phrase.sort(key=lambda x:x.number)
+        phrase = [p for p in phrase if p.syntax != "PUNC"]
+        print(" ".join([p.surface for p in phrase]))
+
+
+# Поиск сверху:
+# NPs = {}
+# in_NP = []
+# root = tree.get_root()
+# print(root.surface)
+# children = tree.get_children(root.number)
+# # print([ch.surface for ch in children])
+# for child in children:
+#     if child.pos == "S":
+#         NP = tree.get_all_children(child.number)
+#         print(" ".join([child.surface] + [n.surface for n in NP]))
